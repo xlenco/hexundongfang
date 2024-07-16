8f925a8a138442001e53c670de34aca63f541a69
xlenco <1043865083qq.com>
2024-07-16 13:41:36 +0800

    122

--- README.md
+++
@@ -1,3 +0,0 @@
-# hexundongfang
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/xlenco/hexundongfang)
---
+++ my-docs/src/.vuepress/config.ts
@@ -0,0 +1,25 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "文档演示",
+      description: "vuepress-theme-hope 的文档演示",
+    },
+    "/en/": {
+      lang: "en-US",
+      title: "Docs Demo",
+      description: "A docs demo for vuepress-theme-hope",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
